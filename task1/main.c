c1e4aaedd687100b722c43957b28d3008dc72563
Mr Madique <neoevil25@gmail.com>
2021-10-04 02:03:10 +0300

    random generation advancements

--- task1/main.c
+++ task1/main.c
@@ -1,4 +1,4 @@
-//main.c is all about input/output
+//main.c is all about input/output and bubble sort processing
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -13,71 +13,103 @@ struct container {
 };
 
 int main(int argc, char * argv[]) {
+	int randomMode = 0;
+	int randomNum = 0;
+	
     //check arguments for validity
-    if (argc == 3) {
-        printf("The filenames supplied are %s and %s\n", argv[1], argv[2]);
-        if (strcmp(argv[1], argv[2]) == 0) {
-            printf("Filenames are the same. Aborting.\n");
+    if (argc == 5) {
+        printf("Output filenames supplied are %s and %s\n", argv[3], argv[4]);
+        if (strcmp(argv[3], argv[4]) == 0) {
+            printf("Output filenames are the same. Aborting.\n");
             return -1;
         }
-    } else if (argc > 3) {
-        printf("Too many arguments supplied. Two arguments expected.\n");
+    } else if (argc > 5) {
+        printf("Too many arguments supplied. Mode and three arguments expected: main <-f in_file/-r rand_key> out_file1 out_file2\n");
         return -1;
     } else {
-        printf("Not enough arguments. Two arguments expected.\n");
+        printf("Not enough arguments. Mode and three arguments expected: main <-f in_file/-r rand_key> out_file1 out_file2\n");
         return -1;
     }
 	
-	//open files for reading, writing, test availability
+	//switch to random mode if "-r"
+	if (strcmp(argv[1], "-r") == 0) {
+		printf("Switching to random mode...\n");
+		randomMode = 1;
+		randomNum = atoi(argv[2]);
+		if ((randomNum < 1) || (randomNum > 10000)) {
+			printf("Random mode failure: please input rand_key in range[1, 10000]\n");
+			return -1;
+		}
+		printf("Random mode initiated %d\n", randomNum);
+	} else if (strcmp(argv[1], "-f") != 0) {
+		printf("Unknown mode. Supported modes are: <-f> for file input and <-r> for random input\n");
+	}
+	
+	//open files for reading, writing
 	printf("opening files now..\n");
-   
-	FILE *in_file = fopen(argv[1], "r");
-	FILE *out_file = fopen(argv[2], "w");
+	
+	FILE *in_file = NULL;
+	if (randomMode == 0) {
+		in_file = fopen(argv[2], "r");
+	}
+	FILE *out_file = fopen(argv[3], "w");
+	FILE *out_file_sort = fopen(argv[4], "w");
 
-	if (in_file == NULL) {   
+	//test access to files
+	if ((in_file == NULL) && (randomMode == 0)) {   
 		printf("Error! Could not open file for reading\n"); 
 		exit(-1);
 	} 
-	if (out_file == NULL) {   
-		printf("Error! Could not open file for writing\n"); 
+	if ((out_file == NULL) || (out_file_sort == NULL)) {   
+		printf("Error! Could not open files for writing\n"); 
 		exit(-1);
 	}
 	
 	printf("files opened successfully\n");
 	
-	//read input file into container, get statistics
-	char sType[20];
-	enum shapeType eType;
-	int a, b, c, r;
-	int damagedData = 0;
-	float tempDens;
-	float totalVol = 0.0;
+	//all arguments checked out, var init time
+	//init container of 10000 shapes, set size to 0
 	struct container cont;
 	cont.size = 0;
+	//init var for temporary shape headed for container
+	char sType[20];
+	enum shapeType eType;
 	struct shape tempShape;
-	while (fscanf(in_file, "%s", sType) != EOF) {
+	//init tracking data
+	int damagedData = 0;
+	float totalVol = 0.0;
+	
+	/*algorithm to generating or reading a shape container
+	create a tempShape:
+		get enumerated type: sphere, paral, or tetra
+		get float of range (0; 10000) for density 
+		get 1 or 3 or 1 ints of range (0; 10000) for dimensions
+		process dimensions into volume through volume()
+	put tempShape into container
+	*/
+	
+	//in case of randomMode, generate the container
+	if (randomMode == 1) {
+		//init var for temporary shape dimensions
+		int tempA, tempB, tempC;
+		float tempDens;
+	}
+	
+	//read input file into container, get statistics
+	while ((randomMode == 0) && (fscanf(in_file, "%s", sType) != EOF)) {
 		eType = strToShape(sType);
+		tempShape.type = eType;
 		//input data
 		switch(eType) {
 			case SPHERE:
-				fscanf(in_file, "%f %d", &tempDens, &r);
-				tempShape.type = eType;
-				tempShape.s.radius = r;
-				tempShape.dens = tempDens;
-				tempShape.vol = volume(&tempShape);
+				fscanf(in_file, "%f %d", &tempShape.dens, &tempShape.s.radius);
 				break;
 			case PARAL:
 				fscanf(in_file, "%f %d %d %d", &tempShape.dens, &tempShape.p.a,
 				&tempShape.p.b, &tempShape.p.c);
-				tempShape.type = eType;
-				tempShape.vol = volume(&tempShape);
 				break;
 			case TETRA:
-				fscanf(in_file, "%f %d", &tempDens, &a);
-				tempShape.type = eType;
-				tempShape.t.a = a;
-				tempShape.dens = tempDens;
-				tempShape.vol = volume(&tempShape);
+				fscanf(in_file, "%f %d", &tempShape.dens, &tempShape.t.a);
 				break;
 			case WRONG:
 				printf("shape damaged");
@@ -85,9 +117,8 @@ int main(int argc, char * argv[]) {
 				break;
 		}
 		if (eType != WRONG) {
+			tempShape.vol = volume(&tempShape);
 			cont.shapes[cont.size] = tempShape;
-			//printf("%s %f %f\n", shapeToStr(cont.shapes[cont.size].type),
-			//cont.shapes[cont.size].dens, cont.shapes[cont.size].vol);
 			cont.size++;
 		}
 	}
